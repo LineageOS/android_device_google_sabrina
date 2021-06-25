981901e9cfc8b29a80648e68ba66969632bf0cfb
Aaron Kling <webgeek1234@gmail.com>
2021-06-25 01:04:25 -0400

    sabrina: Build Broadcom DHD module inline
    Change-Id: I042931d6361420868f53f682c70fee8c5a2b589d

--- device.mk
+++ device.mk
@@ -9,3 +9,7 @@ DEVICE_PATH := device/google/sabrina
 $(call inherit-product, vendor/google/sabrina/sabrina-vendor.mk)
 
 $(call inherit-product, device/amlogic/g12-common/g12.mk)
+
+## Kernel Modules
+PRODUCT_PACKAGES += \
+    dhd
