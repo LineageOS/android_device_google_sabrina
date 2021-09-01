2fe2cc13da12ffa402d0421531293625aaac9335
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-09-01 16:13:30 +0000

    sabrina: build & overlay KeyHandler
    Change-Id: I1bd4c27a646f5deda26f20d99e87ae61f977bde6

--- device.mk
+++ device.mk
@@ -21,10 +21,15 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/init-files/init.amlogic.sku.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.sku.rc \
     $(DEVICE_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
 
+## Keyhandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 ## Kernel Modules
 PRODUCT_PACKAGES += \
     dhd
 
 ## Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
