524c03565b6d22ae27395f30a767e92c6e375ce8
Nolen Johnson <johnsonnolen@gmail.com>
2024-02-02 01:52:38 -0500

    sabrina: sabrina_prod_stable-user 12 STTL.231114.004 11218674 release-keys
    * Unset specific DHD version, the common on is what stock uses now.
    * Include all device-specific rc's from wifi_buildin.rc for convienence.
    * We require our own keymint (ugh, why?).
    
    Change-Id: I9c51a68983b7025b110f6b1bd65a9bcce8fd0bc2

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        # Use generic Light HAL context for led_control_service
+        vendor/etc/init/led_control_service.rc)
+            sed -i "s|u:r:led_control_service:s0|u:object_r:hal_light_default_exec:s0|g" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
