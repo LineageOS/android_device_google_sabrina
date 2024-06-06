dabc119e95c5c01ebd3850439323475636777d56
Nolen Johnson <johnsonnolen@gmail.com>
2024-06-06 01:44:25 -0400

    stash
    Change-Id: I80395a37d025df62d1e6bcd9d542aba7d63eb103

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,8 +32,8 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_AP := "/wifi/fw_bcm4356a2_ag_apsta.bin"
-WIFI_DRIVER_FW_PATH_STA := "/wifi/fw_bcm4356a2_ag.bin"
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/wifi/fw_bcm4356a2_ag_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/wifi/fw_bcm4356a2_ag.bin"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
