e7fde6576eac9d640f3d22d942f9f984f17f20f0
Bruno Martins <bgcngm@gmail.com>
2024-07-14 13:54:23 +0000

    Revert "sabrina: Override systemcontrol to allow NET_ADMIN"
    This reverts commit a627b61631364e94a5af7d6087128120e95aaaca.
    
    Reason for revert: Doesn't work.
    
    Change-Id: Ia272f8644f31dfab2f5d3ee320e936d0ca6f3689

--- device.mk
+++ device.mk
@@ -16,8 +16,7 @@ include kernel/amlogic/kernel-modules/dhd-driver/firmware/bluetooth/bluetooth.mk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.sku.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.sku.rc \
     $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc \
-    $(LOCAL_PATH)/init-files/init.recovery.sabrina.rc:recovery/root/init.recovery.sabrina.rc \
-    $(LOCAL_PATH)/init-files/systemcontrol_sabrina.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/systemcontrol_sabrina.rc
+    $(LOCAL_PATH)/init-files/init.recovery.sabrina.rc:recovery/root/init.recovery.sabrina.rc
 
 ## Keylayout (IR)
 PRODUCT_COPY_FILES += \
