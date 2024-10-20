f51cf4e72bbc4a53b742ff790920d841a9129ca1
Bruno Martins <bgcngm@gmail.com>
2024-10-20 11:35:53 +0100

    sabrina: Adopt Python extract utils
    Change-Id: I56cc92329e15a50a86da2239b91822aee59e8d2b

---
+++ extract-files.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_proto_3_9_1,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/amlogic/sm1-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/hw/android.hardware.security.keymint-service.amlogic': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V3-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so'),
+    'vendor/etc/init/led_control_service.rc': blob_fixup()
+        .regex_replace('  seclabel u:r:led_control_service:s0', ''),
+    'vendor/etc/init/tee-supplicant.rc': blob_fixup()
+        .regex_replace('/vendor/lib/', '/vendor/lib/modules/'),
+}  # fmt: skip
+
+lib_fixups: lib_fixups_user_type = {
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+}
+
+module = ExtractUtilsModule(
+    'sabrina',
+    'google',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, '../amlogic/sm1-common', module.vendor)
+    utils.run()
